7dbbc7cecb5d8d3e6b95812573ccbd06977a5464
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-01-05 01:59:13 -0300

    - removing mf-types

--- @mf-types/remote/Button.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/src/components/ui/button';
-export { default } from './compiled-types/src/components/ui/button';
--- @mf-types/remote/apis.d.ts
+++
@@ -1,6 +0,0 @@
-export type RemoteKeys = 'remote/Button' | 'remote/Components';
-type PackageType<T> = T extends 'remote/Components'
-  ? typeof import('remote/Components')
-  : T extends 'remote/Button'
-    ? typeof import('remote/Button')
-    : any;
